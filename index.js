fcdea3e513d0e221b77aaaa4bad6cfec35bebcd4
Freddie <fthompson803@live.com>
2020-01-24 00:11:36 -0500

    changes again

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const server = require('./server.js');
 
 const port = process.env.PORT;
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'Api is up and running' });
-});
 server.listen(port, () => {
   console.log('Server listening on Port 5000');
 });
